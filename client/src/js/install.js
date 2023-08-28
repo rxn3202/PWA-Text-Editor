023499c26916fade5cbb83cb6aa009aff5d21124
rxn3202 <126435652+rxn3202@users.noreply.github.com>
2023-08-28 01:53:02 -0700

    implemented a click event handler on the butInstall element

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -11,8 +11,21 @@ window.addEventListener('beforeinstallprompt', (event) => {});
     butInstall.classList.toggle('hidden', false);
 
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+// Implement a click event handler on the `butInstall` element
+butInstall.addEventListener('click', async () => {
+    const promptEvent = window.deferredPrompt;
+    if (!prompEvent){
+        return;
+    }
+
+    // Show prompt
+    prompEvent.prompt();
+
+    // Reset the deferred prompt variable. It can only be used once.
+    window.deferredPrompt = null;
+
+    butInstall.classList.toggle('hidden', true);
+});
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {});
